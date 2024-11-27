7bbc195b33afdbd4587f7dd74578dbfc0353e148
Himanshu <himanshurule03@gmail.com>
2024-11-28 02:09:37 +0530

    changed the host and port for hosting as per the render requirements

--- main.py
+++ main.py
@@ -25,5 +25,7 @@ def index():
 def serve_file(filename):
     return send_from_directory(os.path.join(app.root_path, 'static'), filename)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    # Get the port from the environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
