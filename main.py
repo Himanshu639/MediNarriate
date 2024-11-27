d4e70d40701696ce2068e77dc8816d5cea6f6627
Himanshu <himanshurule03@gmail.com>
2024-11-28 01:55:35 +0530

    replaced coqui-tts with gtts

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from utils.mistral_ai import mistral_ai
-from TTS.api import TTS
+from gtts import gTTS
 from flask import Flask, render_template, request, send_from_directory
 import os
 
 
 app = Flask(__name__)
-tts = TTS('tts_models/en/ljspeech/speedy-speech')
-tts.to('cuda')
 gpt = mistral_ai()
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,7 +14,8 @@ def index():
         # Get text input
 		user_text = request.form["text_input"]
 		res = gpt.chat_completion('Explain this in a better way to understand (with no punctuations please, only text)\n'+user_text,3)
-		tts.tts_to_file(res,file_path='./static/output.wav')
+		# tts.tts_to_file(res,file_path='./static/output.wav')
+		gTTS(res).save("./static/output.wav")
 		is_audio_file = True
 
 		return render_template("index.html", audio_file=is_audio_file)
--- setup.py
+++
@@ -1,2 +0,0 @@
-from TTS.api import TTS
-tts = TTS('tts_models/en/ljspeech/tacotron2-DDC_ph')
