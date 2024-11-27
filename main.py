8b66ecbf2271ba486002d0465758cfc37882a4c8
Himanshu <himanshurule03@gmail.com>
2024-11-28 02:46:55 +0530

    added new page for playing audio

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 from utils.mistral_ai import mistral_ai
 from gtts import gTTS
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import os
-
 
 app = Flask(__name__)
 gpt = mistral_ai()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	is_audio_file = False
-	if request.method == "POST":
+    if request.method == "POST":
         # Get text input
-		user_text = request.form["text_input"]
-		res = gpt.chat_completion('Explain this in a better way to understand (with no punctuations please, only text)\n'+user_text,3)
-		# tts.tts_to_file(res,file_path='./static/output.wav')
-		gTTS(res).save("./static/output.wav")
-		is_audio_file = True
+        user_text = request.form["text_input"]
+        res = gpt.chat_completion('Explain this in a better way to understand (with no punctuations please, only text)\n'+user_text, 3)
+        
+        # Save the audio file
+        gTTS(res).save("./static/output.wav")
+        
+        # Redirect to the audio page
+        return redirect(url_for('audio'))
 
-		return render_template("index.html", audio_file=is_audio_file)
-	return render_template("index.html", audio_file=is_audio_file)
+    return render_template("index.html")
+
+@app.route("/audio")
+def audio():
+    # Pass the filename to the template to be used in the audio player
+    return render_template("audio.html", filename='output.wav')
 
 @app.route("/static/<filename>")
 def serve_file(filename):
+    # Make sure the path is correctly referenced
     return send_from_directory(os.path.join(app.root_path, 'static'), filename)
 
 if __name__ == '__main__':
